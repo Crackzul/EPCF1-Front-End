4dcbc17b08662f2d116516f4b55cf3cbeb36ac48
Didier Marty <boutcamp@gmail.com>
2025-04-27 10:29:35 +0200

    au cas ou..

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger'); // Cherche le bouton burger
 const nav = document.querySelector('nav ul'); // Cherche mon menu nav ul
 
-burger.addEventListener('click', () => { // Quand je clique sur le "bouton" il ajoute ou enlève la classe open sur nav ul
+burger.addEventListener('click', () => { // Quand je clique sur le "bouton",
+//  ajoute ou enlève la classe open sur nav ul
+
   nav.classList.toggle('open'); // ajoute "dynamiquement la classe .open"
 });
